a4cf8ed86d550e78cae747a577e2a4910be1a57c
Vicente Lucero <vjlucero@miuandes.cl>
2018-05-22 12:34:28 -0400

    intento de alarma no funcional

--- app/src/main/java/AlarmReceiver.java
+++ app/src/main/java/AlarmReceiver.java
@@ -1,10 +1,15 @@
+import android.app.AlarmManager;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
+import com.example.gamma.lab07.MainActivity;
 import com.example.gamma.lab07.R;
+
+import static android.content.Context.ALARM_SERVICE;
 
 
 public class AlarmReceiver extends BroadcastReceiver {
@@ -12,29 +17,32 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
+        AlarmManager alarmManager=(AlarmManager) context.getSystemService();
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,System.currentTimeMillis(),2000, pendingIntent);
 
+    }
 
-        int id = 1;
-        private void generateNotification() {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"channelID")
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setContentTitle("alarma")
-                    .setContentText("la alarma esta sonando..");
+    int id = 1;
+    private void generateNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"channelID")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("alarma")
+                .setContentText("la alarma esta sonando..");
 
-            NotificationManager notificationManager=(NotificationManager) context.getSystemService();
-            notificationManager.notify(id,builder.build());
-            id++;
-        }
+        NotificationManager notificationManager=(NotificationManager) context.getSystemService();
+        notificationManager.notify(id,builder.build());
+        id++;
+    }
 
-        private void updateNotification(){
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"channelID")
-                    .setSmallIcon(R.drawable.ic_launcher_background)
-                    .setContentTitle("alarma")
-                    .setContentText("la alarma esta sonando..");
-            NotificationManager notificationManager=(NotificationManager) context.getSystemService();
-            notificationManager.notify(id,builder.build());
-            id++;
-        }
-
+    private void updateNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,"channelID")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("alarma")
+                .setContentText("la alarma esta sonando..");
+        NotificationManager notificationManager=(NotificationManager) context.getSystemService();
+        notificationManager.notify(id,builder.build());
+        id++;
     }
 }
